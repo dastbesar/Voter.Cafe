5165fa4ef9951df6ca8ebacd29cad9b9fb368648
Avisha <avisha.sabaghian@gmail.com>
2018-10-04 21:08:13 -0700

    port

--- app.js
+++ app.js
@@ -118,6 +118,6 @@ app.use(function (req, res){
   res.render('404');
 });
 
-app.listen(app.get('port'), function(){
-  console.log("Express started on http://localhost:" + app.get('port') + 'press Ctrl-C to terminate');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
